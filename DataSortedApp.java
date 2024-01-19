dd96d4450ee2fc02124d8add088d3fd719423a3b
borisweb2020 <49887284+borisweb2020@users.noreply.github.com>
2024-01-19 13:50:41 +0700

    Implement sorting the data into integers and strings based on their types

--- DataSortedApp.java
+++ DataSortedApp.java
@@ -7,23 +7,26 @@ import java.io.IOException;
 public class DataSortedApp{
 	public static void main(String[] args){
 		String inputFileName = "resources/input/in1.txt";
-		String outputFileName = "resources/output/out_integer.txt";
+		String intOutputFileName = "resources/output/out_integer.txt";
+		String stringOutputFileName = "resources/output/out_string.txt";
 
 		try(BufferedReader reader = new BufferedReader(new FileReader(inputFileName));
-				BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName))){
+				BufferedWriter intWriter = new BufferedWriter(new FileWriter(intOutputFileName));
+				BufferedWriter stringWriter = new BufferedWriter(new FileWriter(stringOutputFileName))){
 
 			String line;
 			while((line = reader.readLine()) != null){
 				try {
-					Integer.parseInt(line);
-					writer.write(line);
-					writer.newLine();
+					Long.parseLong(line);
+					intWriter.write(line);
+					intWriter.newLine();
 				} catch(NumberFormatException e) {
-					// Ignoring...
+					stringWriter.write(line);
+					stringWriter.newLine();
 				}
 			}
 
-			System.out.println("Filtering completed. Check the file: " + outputFileName);
+			System.out.println("Filtering completed. Check the files.");
 		} catch(IOException e) {
 			System.out.println("An error occurred: ");
 			e.printStackTrace();
