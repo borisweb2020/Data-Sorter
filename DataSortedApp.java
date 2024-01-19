e0f5ca56649c4d7039fa6ede39fec80816f6c164
borisweb2020 <49887284+borisweb2020@users.noreply.github.com>
2024-01-19 18:56:13 +0700

    Implement writing data to files by appending to existing ones

--- DataSortedApp.java
+++ DataSortedApp.java
@@ -6,44 +6,49 @@ import java.io.IOException;
 
 public class DataSortedApp{
 	public static void main(String[] args){
-		if(args.length == 0){
-			System.out.println("Input a name file");
-			return;
-		}
+		String intOutputFileName = "resources/output/out_integer.txt";
+		String floatOutputFileName = "resources/output/out_float.txt";
+		String stringOutputFileName = "resources/output/out_string.txt";
 
-		for(String fileName : args) {
-			String inputFileName = "resources/input/" + fileName;
-			String intOutputFileName = "resources/output/out_integer.txt";
-			String floatOutputFileName = "resources/output/out_float.txt";
-			String stringOutputFileName = "resources/output/out_string.txt";
+		try(BufferedWriter intWriter = new BufferedWriter(new FileWriter(intOutputFileName, true));
+				BufferedWriter floatWriter = new BufferedWriter(new FileWriter(floatOutputFileName, true));
+				BufferedWriter stringWriter = new BufferedWriter(new FileWriter(stringOutputFileName, true))){
 
-			try(BufferedReader reader = new BufferedReader(new FileReader(inputFileName));
-					BufferedWriter intWriter = new BufferedWriter(new FileWriter(intOutputFileName));
-					BufferedWriter floatWriter = new BufferedWriter(new FileWriter(floatOutputFileName));
-					BufferedWriter stringWriter = new BufferedWriter(new FileWriter(stringOutputFileName))){
+			if(args.length == 0){
+				System.out.println("CLI correct input: java DataSortedApp in1.txt in2.txt");
+				return;
+			}
 
-				String line;
-				while((line = reader.readLine()) != null){
-					try {
-						Long.parseLong(line);
-						intWriter.write(line);
-						intWriter.newLine();
-					} catch(NumberFormatException e1) {
+			for(String fileName : args) {
+				String inputFileName = "resources/input/" + fileName;
+				try(BufferedReader reader = new BufferedReader(new FileReader(inputFileName))){
+					String line;
+					while((line = reader.readLine()) != null){
 						try {
-							Double.parseDouble(line);
-							floatWriter.write(line);
-							floatWriter.newLine();
-						} catch(NumberFormatException e2) {
-							stringWriter.write(line);
-							stringWriter.newLine();
+							Long.parseLong(line);
+							intWriter.write(line);
+							intWriter.newLine();
+						} catch(NumberFormatException e1) {
+							try {
+								Double.parseDouble(line);
+								floatWriter.write(line);
+								floatWriter.newLine();
+							} catch(NumberFormatException e2) {
+								stringWriter.write(line);
+								stringWriter.newLine();
+							}
 						}
 					}
+					System.out.println("Filtering completed. Check the files.");
+				} catch(IOException e) {
+					System.out.println("An error occurred: ");
+					e.printStackTrace();
 				}
-				System.out.println("Filtering completed. Check the files.");
-			} catch(IOException e) {
-				System.out.println("An error occurred: ");
-				e.printStackTrace();
 			}
+		} catch(IOException e) {
+			System.out.println("An error occurred: ");
+			e.printStackTrace();
 		}
+
 	}
 }
